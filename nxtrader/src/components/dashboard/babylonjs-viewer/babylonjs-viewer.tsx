f706c31ae60cfcb4f272f729f79d6807a4fe36c4
Jose Fajardo <jose.fajardo@oracle.com>
2024-05-04 19:58:57 +1000

    babylonjs

--- nxtrader/src/components/dashboard/babylonjs-viewer/babylonjs-viewer.tsx
+++ nxtrader/src/components/dashboard/babylonjs-viewer/babylonjs-viewer.tsx
@@ -1,7 +1,11 @@
 import { ExtendGlobalProps, registerCustomElement } from "ojs/ojvcomponent";
 import { h, ComponentProps, ComponentType } from "preact";
+import { useEffect} from "preact/hooks";
 import componentStrings = require("ojL10n!./resources/nls/babylonjs-viewer-strings");
 import "css!dashboard/babylonjs-viewer/babylonjs-viewer-styles.css";
+import * as BABYLON from 'babylonjs';
+import * as Context from "ojs/ojcontext";
+//import { Scene, Engine } from 'babylonjs';
 
 type Props = Readonly<{
   message?: string;
@@ -17,7 +21,57 @@ type Props = Readonly<{
 function BabylonjsViewerImpl(
   { message = "Hello from  dashboard-babylonjs-viewer" }: Props
 ) {
-  return <p>{message}</p>
+
+  function initBabs() {
+    
+    var canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+    const busyContext = Context.getContext(canvas).getBusyContext();
+    busyContext.whenReady().then(() => {
+      var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true });
+
+      console.log("babylonjs-viewer > initBabs", canvas, engine);
+
+      // CreateScene function that creates and return the scene
+      var createScene = function(){
+        // Create a basic BJS Scene object
+        var scene = new BABYLON.Scene(engine);
+        // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
+        var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+        // Target the camera to scene origin
+        camera.setTarget(BABYLON.Vector3.Zero());
+        // Attach the camera to the canvas
+        camera.attachControl(canvas, false);
+        // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
+        var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+        // Create a built-in "sphere" shape using the SphereBuilder
+        var sphere = BABYLON.MeshBuilder.CreateSphere('sphere1', {segments: 16, diameter: 2, sideOrientation: BABYLON.Mesh.FRONTSIDE}, scene);
+        // Move the sphere upward 1/2 of its height
+        sphere.position.y = 1;
+        // Create a built-in "ground" shape;
+        var ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 6, height: 6, subdivisions: 2, updatable: false }, scene);
+        // Return the created scene
+        return scene;
+      }
+      // call the createScene function
+      var scene = createScene();
+      // run the render loop
+      engine.runRenderLoop(function(){
+          scene.render();
+      });
+      // the canvas/window resize event handler
+      window.addEventListener('resize', function(){
+        engine.resize();
+      });
+    });
+  }
+
+  useEffect(()=>{
+    initBabs();
+  }, []);
+
+  return <div>
+    <canvas id="renderCanvas"></canvas>
+  </div>
 }
 
 export const BabylonjsViewer: ComponentType <
--- nxtrader/web/components/dashboard/1.0.0/babylonjs-viewer/babylonjs-viewer.js
+++ nxtrader/web/components/dashboard/1.0.0/babylonjs-viewer/babylonjs-viewer.js
@@ -1,9 +1,38 @@
-define(["require", "exports", "preact/jsx-runtime", "ojs/ojvcomponent", "css!dashboard/babylonjs-viewer/babylonjs-viewer-styles.css"], function (require, exports, jsx_runtime_1, ojvcomponent_1) {
+define(["require", "exports", "preact/jsx-runtime", "ojs/ojvcomponent", "preact/hooks", "babylonjs", "ojs/ojcontext", "css!dashboard/babylonjs-viewer/babylonjs-viewer-styles.css"], function (require, exports, jsx_runtime_1, ojvcomponent_1, hooks_1, BABYLON, Context) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     exports.BabylonjsViewer = void 0;
     function BabylonjsViewerImpl({ message = "Hello from  dashboard-babylonjs-viewer" }) {
-        return (0, jsx_runtime_1.jsx)("p", { children: message });
+        function initBabs() {
+            var canvas = document.getElementById('renderCanvas');
+            const busyContext = Context.getContext(canvas).getBusyContext();
+            busyContext.whenReady().then(() => {
+                var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
+                console.log("babylonjs-viewer > initBabs", canvas, engine);
+                var createScene = function () {
+                    var scene = new BABYLON.Scene(engine);
+                    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+                    camera.setTarget(BABYLON.Vector3.Zero());
+                    camera.attachControl(canvas, false);
+                    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+                    var sphere = BABYLON.MeshBuilder.CreateSphere('sphere1', { segments: 16, diameter: 2, sideOrientation: BABYLON.Mesh.FRONTSIDE }, scene);
+                    sphere.position.y = 1;
+                    var ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 6, height: 6, subdivisions: 2, updatable: false }, scene);
+                    return scene;
+                };
+                var scene = createScene();
+                engine.runRenderLoop(function () {
+                    scene.render();
+                });
+                window.addEventListener('resize', function () {
+                    engine.resize();
+                });
+            });
+        }
+        (0, hooks_1.useEffect)(() => {
+            initBabs();
+        }, []);
+        return (0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("canvas", { id: "renderCanvas" }) });
     }
     exports.BabylonjsViewer = (0, ojvcomponent_1.registerCustomElement)("dashboard-babylonjs-viewer", BabylonjsViewerImpl, "BabylonjsViewer", { "properties": { "message": { "type": "string" } } }, { "message": "Hello from  dashboard-babylonjs-viewer" });
 });
